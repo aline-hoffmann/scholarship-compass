b04d381f9e51d910701d29f10ff4ae9f782dbee9
aline-hoffmann <alinehoffmannp@gmail.com>
2025-08-16 18:54:54 -0300

    sqls das etapas 1 e 2 do desafio concluídos.

--- Sprint_1/Desafio/etapa1.sql
+++ Sprint_1/Desafio/etapa1.sql
@@ -1,19 +1,20 @@
+--Visualizando a tabela original (tb_locacao)
+
 select *
 from tb_locacao
+
+-- Renomeando e visualizando a tabela original
 
 alter table tb_locacao rename to tb_original
 
 select *
 from tb_original
 
-update tb_original
+/* update tb_original
 set dataLocacao = substring(dataLocacao, 1, 4) || '-' || substring(dataLocacao, 5, 2) || '-' || substring(dataLocacao, 7, 2),
-	dataEntrega = substring(dataEntrega , 1, 4) || '-' || substring(dataEntrega , 5, 2) || '-' || substring(dataEntrega , 7, 2)
-	
-select *
-from tb_original
+	dataEntrega = substring(dataEntrega , 1, 4) || '-' || substring(dataEntrega , 5, 2) || '-' || substring(dataEntrega , 7, 2)*/
 
--- Criando a tabela vendedor
+-- Criando e visualizando a tabela vendedor
 
 create table vendedor(
 	idVendedor int primary key,
@@ -21,8 +22,11 @@ create table vendedor(
 	sexoVendedor smallint,
 	estadoVendedor varchar
 	)
+
+select *
+from vendedor
 	
--- Criando a tabela cliente
+-- Criando e visualizando a tabela cliente
 	
 create table cliente(
 	idCliente int primary key,
@@ -31,15 +35,21 @@ create table cliente(
 	estadoCliente varchar,
 	paisCliente varchar
 	)
+
+select *
+from cliente
 	
--- Criando a tabela combustivel
+-- Criando e visualizando a tabela combustivel
 	
 create table combustivel(
 	idCombustivel int primary key,
 	tipoCombustivel varchar
 	)
+
+select *
+from combustivel
 	
--- Criando a tabela carro
+-- Criando e visualizando a tabela carro
 	
 create table carro(
 	idCarro int primary key,
@@ -50,12 +60,15 @@ create table carro(
 	anoCarro int,
 	idCombustivel int references combustivel(idCombustivel)
 	)
+
+select *
+from carro
 	
--- Criando a tabela locacao
+-- Criando e visualizando a tabela locacao
 	
 create table locacao(
 	idLocacao int primary key,
-	dataLocacao date,
+	dataLocacao datetime,
 	horaLocacao time,
 	qntDiaria int,
 	vlrDiaria decimal(6,2),
@@ -64,26 +77,31 @@ create table locacao(
 	idCarro int references carro(idCarro)
 	)
 
+select *
+from locacao
 
--- Criando a tabela entrega_loc
+
+-- Criando e visualizando a tabela entrega_loc
 
 create table entrega_loc(
-	idLocacao int primary key references locacao(idLocacao)
-	dataEntrega date,
+	idLocacao int primary key references locacao(idLocacao),
+	dataEntrega datetime,
 	horaEntrega time
 	)
 
+select *
+from entrega_loc
 
-	-- Inserindo dados na tabela vendedor
+-- Inserindo dados na tabela vendedor
 
 insert into vendedor(idVendedor,nomeVendedor, sexoVendedor, estadoVendedor)
-select distinct idVendedor,nomeVendedor, sexoVendedor, estadoVendedor
+select distinct idVendedor, nomeVendedor, sexoVendedor, estadoVendedor
 from tb_original 
 
--- Visualizando e criando um "apelido" para a tabela vendedor
+-- Visualizando a tabela vendedor
 
 select *
-from vendedor as vend
+from vendedor
 
 -- Inserindo dados na tabela cliente
 
@@ -91,10 +109,10 @@ insert into cliente(idCliente, nomeCliente, CidadeCliente, estadoCliente, paisCl
 select distinct idCliente, nomeCliente, cidadeCliente, estadoCliente, paisCliente
 from tb_original 
 
--- Visualizando e criando um "apelido" para a tabela cliente
+-- Visualizando a tabela cliente
 
 select *
-from cliente as cli
+from cliente
 
 -- Inserindo dados na tabela combustivel
 
@@ -102,26 +120,21 @@ insert into combustivel (idCombustivel, tipoCombustivel)
 select distinct idcombustivel, tipoCombustivel
 from tb_original 
 
--- Visualizando e criando um "apelido" para a tabela combustivel
+-- Visualizando a tabela combustivel
 
 select *
-from combustivel as comb
+from combustivel
 
 -- Inserindo dados na tabela carro
 
 insert into carro(idCarro, kmCarro, chassiCarro, marcaCarro, modeloCarro, anoCarro, idCombustivel)
-select distinct idCarro, kmCarro, chassiCarro, marcaCarro, modeloCarro, anoCarro, idCombustivel 
+select distinct idCarro, kmCarro, chassiCarro, marcaCarro, modeloCarro, anoCarro, idcombustivel 
 from tb_original 
 
--- Visualizando e criando um "apelido" para a tabela carro
-
-select *
-from carro as car
-
--- Ocorreu um erro, pois existem carros com mais de uma kilometragem. Sendo assim, optei por selecionar apenas a máxima kilometragem do carro, deixando a query assim:
+-- Ocorreu um erro nessa inserção de dados, então verifiquei que existem carros com mais de uma kilometragem. Sendo assim, optei por selecionar apenas a máxima kilometragem de cada carro, deixando a query assim:
 
 insert into carro(idCarro, kmCarro, chassiCarro, marcaCarro, modeloCarro, anoCarro, idCombustivel)
-select distinct idCarro, kmCarro, chassiCarro, marcaCarro, modeloCarro, anoCarro, idCombustivel 
+select distinct idCarro, kmCarro, chassiCarro, marcaCarro, modeloCarro, anoCarro, idcombustivel 
 from tb_original 
 where kmCarro = (
 	select(kmCarro)
@@ -129,26 +142,57 @@ where kmCarro = (
 	where maxkm.idCarro = tb_original.idCarro
 ) group by idCarro
 
--- Inserindo dados na tabela locacao
-
-insert into locacao(dataLocacao, horaLocacao, qntDiaria, vlrDiaria, idVendedor, idCiente, idCarro)
-select distinct dataLocacao, horaLocacao, qtdDiaria, vlrDiaria, idVendedor, idCliente, idCarro
-from tb_original 
-
--- Visualizando e criando um "apelido" para a tabela locacao
+-- Visualizando a tabela carro
 
 select *
-from locacao as loc
+from carro 
 
--- Verifiquei que houve um erro de digitação na coluna "IdCiente" e realizei a correção
+-- Inserindo dados na tabela locacao
 
-alter table locacao rename column idCiente to idCliente
+insert into locacao(idLocacao, dataLocacao, horaLocacao, qntDiaria, vlrDiaria, idVendedor, idCiente, idCarro)
+select distinct idLocacao, dataLocacao, horaLocacao, qtdDiaria, vlrDiaria, idVendedor, idCliente, idCarro
+from tb_original 
+
+-- Houve um erro nessa inserção. Verifiquei que houve um erro de digitação na coluna "IdCiente" e realizei a correção. Após isso, utilizei a mesma query para inserir os dados na tabela.
+
+alter table locacao rename column idCiente to idCliente 
+
+insert into locacao(idLocacao, dataLocacao, horaLocacao, qntDiaria, vlrDiaria, idVendedor, idCiente, idCarro)
+select distinct idLocacao, dataLocacao, horaLocacao, qtdDiaria, vlrDiaria, idVendedor, idCliente, idCarro
+from tb_original 
+
+-- Visualizando a tabela locacao
+
+select *
+from locacao 
 
 -- Inserindo dados na tabela entrega_loc
 
 insert into entrega_loc(idLocacao, dataEntrega, horaEntrega)
 select distinct idLocacao, dataEntrega, horaEntrega
 from tb_original
+
+-- Verifiquei que as datas não estavam no formato padrão, ficando pouco inteligíveis. Dessa forma, optei por alterar para o formato YYYY-MM-DD.
+
+-- Alterando o formato das datas na tabela entrega_loc
+
+update entrega_loc
+set dataEntrega = substring(dataEntrega, 1, 4) || '-' || substring(dataEntrega, 5, 2) || '-' || substring(dataEntrega, 7, 2)
+
+-- Visualizando a mudança na tabela entrega_loc
+
+select *
+from entrega_loc
+
+-- Alterando o formato das datas na tabela locacao 
+
+update locacao
+set dataLocacao = substring(dataLocacao, 1, 4) || '-' || substring(dataLocacao, 5, 2) || '-' || substring(dataLocacao, 7, 2)
+
+--  Visualizando a mudança na tabela locacao
+
+select *
+from locacao
 
 -- Excluindo a tb_original
 
--- Sprint_1/Desafio/etapa2.sql
+++ Sprint_1/Desafio/etapa2.sql
@@ -1,6 +1,6 @@
 -- Criando as tabelas para a modelagem dimensional
 
--- Dimensão vendedor
+-- Criando e visuando Dimensão vendedor
 
 create table dim_vendedor(
 	id int primary key,
@@ -9,7 +9,10 @@ create table dim_vendedor(
 	estado varchar
 	)
 
--- Dimensão cliente
+select *
+from dim_vendedor
+
+-- Criando e visuando Dimensão cliente
 
 create table dim_cliente(
 	id int primary key,
@@ -19,8 +22,10 @@ create table dim_cliente(
 	pais varchar
 	)
 
+select *
+from dim_cliente
 	
--- Dimensão carro
+-- Criando e visuando Dimensão carro
 -- Alterei os dados de combustível, trazendo os mesmos para a dimensão carro, sendo assim um atributo.
 
 create table dim_carro(
@@ -34,38 +39,53 @@ create table dim_carro(
 	tipoCombustivel varchar
 	)
 
+select *
+from dim_carro
+
 -- Criando a Tabela fato locacao
 -- Aqui também realizei uma alteração em relação ao modelo relacional, trazendo os dados de entrega para a tabela fato de locacão. Sendo assim atributos dela.
 -- Criei uma chave primária composta, sendo assim, o banco só vai permitir um único registro para cada combinação dos valores idCliente, idVendedor, idCarro e dataLocacao.
 
 create table fato_locacao(
-	idCliente int references cliente(id),
-	idVendedor int references vendedor(id),
-	idCarro int references carro(id),
-	dataLocacao date,
+	idCliente int,
+	idVendedor int,
+	idCarro int,
+	dataLocacao datetime,
 	horaLocacao time,
-	dataEntrega date,
+	dataEntrega datetime,
 	horaEntrega time,
 	qntDiaria int,
 	vlrDiaria decimal(6,2),
 	
 	constraint pk_fato_locacao primary key (idCliente, idVendedor, idCarro, dataLocacao)
 	
+	constraint fk_ft_cliente foreign key (idCliente) references dim_cliente(id),
+	
+	constraint fk_ft_vendedor foreign key (idVendedor) references dim_vendedor(id),
+	
+	constraint fk_ft_carro foreign key (idCarro) references dim_carro(id)
+	
 	)
 
--- Inserindo dados na tabela dim_vendedor
+-- Inserindo dados na tabela dim_vendedor e visualizando
 
 insert into dim_vendedor (id, nome, sexo, estado)
 select distinct idVendedor, nomeVendedor, sexoVendedor, estadoVendedor
 from vendedor
 
--- Inserindo dados na tabela dim_cliente
+select *
+from dim_vendedor
+
+-- Inserindo dados na tabela dim_cliente e visualizando
 
 insert into dim_cliente (id, nome, cidade, estado, pais)
 select distinct idCliente, nomeCliente, cidadeCliente, estadoCliente, paisCliente
 from cliente
 
--- Inserindo dados na tabela dim_carro
+select *
+from dim_cliente
+
+-- Inserindo dados na tabela dim_carro e visualizando
 
 insert into dim_carro(id, km, chassi, marca, modelo, ano, idCombustivel, tipoCombustivel)
 select distinct 
@@ -80,7 +100,10 @@ select distinct
 from carro as c
 join combustivel cm on c.idCombustivel = cm.idCombustivel
 
--- Inserindo dados na tabela fato_locacao
+select *
+from dim_carro
+
+-- Inserindo dados na tabela fato_locacao e visualizando
 
 insert into fato_locacao (idCliente, idVendedor, idCarro, dataLocacao, horaLocacao, dataEntrega, horaEntrega, qntDiaria, vlrDiaria)
 select distinct 
@@ -94,7 +117,11 @@ select distinct
     l.qntDiaria,
     l.vlrDiaria
 from locacao as l
-join entrega_loc as e on l.idLocacao = e.idLocacao
+left join entrega_loc as e on l.idLocacao = e.idLocacao 
+
+select *
+from fato_locacao
+
 
 
 
